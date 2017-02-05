54161b9e426b2123d94244c3c18809fde44657ab
doshmajhan <cpc1007@rit.edu>
2017-02-04 22:39:23 -0500

    Added code to scan for a proxy, working on making it more efficient

--- Homework1/proxy_check.py
+++ Homework1/proxy_check.py
@@ -4,6 +4,7 @@
 """
 
 import argparse
+import netaddr
 import requests
 
 def scan(address):
@@ -13,6 +14,37 @@ def scan(address):
         :param address: The address/range to scan for
         :return proxy_list: the list of proxies that were found
     """
+    proxy_list = []
+    target_url = "http://google.com"
+    base_status = requests.get(target_url)
+    if base_status.status_code != 200:
+        print "Site down"
+        return None
+
+    ports = [80, 888, 800, 880, 808]
+    for num in range(1000, 10000):
+        ports.append(num)
+
+    for ip in netaddr.IPNetwork(address):
+        print(str(ip))
+        for port in ports:
+            proxies = {
+                'http': str(ip) + ":" + str(port),
+                'https': str(ip) + ":" + str(port)
+            }
+
+            try:
+                test_status = requests.get(target_url, proxies=proxies, timeout=4)
+
+            except (requests.exceptions.ConnectionError, requests.exceptions.Timeout):
+                print "%s:%s - not a proxy" % (str(ip), str(port))
+
+            if test_status.status_code == 200:
+                print "%s:%s - IS a proxy: %d" % (str(ip), str(port), int(test_status.status_code))
+                proxy_list += [str(ip) + ":" + str(port)]
+                break
+
+        return proxy_list
 
 
 def main():
@@ -21,7 +53,7 @@ def main():
     """
     parser = argparse.ArgumentParser(description="Scan a range of" \
                                     " IP's for a proxy")
-    parser.add_argument('-i', dest='address', help='The address/range to scan',
+    parser.add_argument('-a', dest='address', help='The address/range to scan',
                         required=True)
 
     args = parser.parse_args()
