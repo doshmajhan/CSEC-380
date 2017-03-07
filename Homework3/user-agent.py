d87ad5de9fbe16f16f1c541bd8fb730a1be4977e
doshmajhan <cpc1007@rit.edu>
2017-03-06 23:16:12 -0500

    Replacing requests usage with sockets

--- Homework3/user-agent.py
+++ Homework3/user-agent.py
@@ -1,5 +1,6 @@
 import csv
 import requests
+import socket
 import threading
 from bs4 import BeautifulSoup
 
@@ -7,54 +8,94 @@ from bs4 import BeautifulSoup
 all_links = set()
 all_emails = set() 
 
+def get_data(sock, request):
+    data = ""
+    sock.send(request)
+    while True:
+        d = sock.recv(1024)
+        if not d:
+            break
+        data += d
+    
+    sock.shutdown(1)
+    sock.close()
+    return data
 
-def get_image(num, items):
+
+def get_image(num, items, ip):
     length = (num*7)+7
     if num == 9:
         length = len(items)
-        
+
     lst = items[num*7:length]
     for img in lst:
         name = img.rsplit('/', 1)[-1]
         print 'Downloading: %s' % name
-        r = requests.get('http://www.rit.edu/%s' % img, stream=True)
-        with open('pics/%s'%name, 'wb') as f:
-            for chunk in r:
-                f.write(chunk)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((ip, 80))
+        img = img.replace(" ", "%20")
+        request = "GET {} HTTP/1.1\r\n" \
+                "Connection: keep-alive\r\n" \
+                "User-Agent: python-requests/2.13.0\r\n" \
+                "Host: www.rit.edu\r\n" \
+                "Accept: image/jpeg,*/*\r\n\r\n".format("http://www.rit.edu"+img)
+
+        data = get_data(s, request)
+        data = data.rsplit('\r\n\r\n')
+        with open('pics/%s' % name, 'wb') as f:
+            f.write(data[1])
 
 
 def activity1():
     url = "https://www.rit.edu/programs/computing-security-bs"
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, 'lxml')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ip = socket.gethostbyname("www.rit.edu")
+    s.connect((ip, 80))
+    request = "GET {} HTTP/1.1\r\n" \
+            "Connection: keep-alive\r\n" \
+            "User-Agent: python-requests/2.13.0\r\n" \
+            "Host: www.rit.edu\r\n" \
+            "Accept: */*\r\n\r\n".format(url)
+
+    data = get_data(s, request)
+    soup = BeautifulSoup(data, 'lxml')
     all_rows = soup.findAll('tr')
     for row in all_rows:
-        tds = row.findAll('td') 
+        tds = row.findAll('td')
         if len(tds) == 3:
             td1 = tds[0].get_text()
             td2 = tds[1].get_text()
             if td1 == u'\xa0':
-                continue     
+                continue
 
-            print td1, td2
-    
+            #print td1, td2
+
     url = "https://www.rit.edu/gccis/computingsecurity/people"
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, 'lxml')
+    request = "GET {} HTTP/1.1\r\n" \
+        "Connection: keep-alive\r\n" \
+        "User-Agent: python-requests/2.13.0\r\n" \
+        "Host: www.rit.edu\r\n" \
+        "Accept: */*\r\n\r\n".format(url)
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, 80))
+    data = get_data(s, request)
+    
+    soup = BeautifulSoup(data, 'lxml')
     divs = soup.findAll('div', {'class': 'staff-picture'})
     pics = []
     for d in divs:
         pic = d.findAll('img')
         pics += [pic[0]['src']]
- 
+
     threads = []
     for i in range(10):
-        t = threading.Thread(target=get_image, args=(i, pics))
+        t = threading.Thread(target=get_image, args=(i, pics, ip))
         threads.append(t)
         t.start()
 
 
-def activity2(domain, url, depth):
+def activity2(domain, url, depth, f=None):
     global all_links
     global all_emails
 
@@ -83,6 +124,9 @@ def activity2(domain, url, depth):
         return None
 
     all_links.add(url)
+    if f:
+        f.write('{}\n'.format(url))
+
     soup = BeautifulSoup(r.content, 'lxml')
     links = soup.findAll('a', href=True)
     emails = soup.select('a[href^=mailto]')
@@ -94,18 +138,23 @@ def activity2(domain, url, depth):
     for l in links:
         print '{} -> {} ==> {}'.format(r.status_code, l['href'], depth)
         activity2(domain, l['href'], depth-1)
-        
+
 
 def activity3():
     f = open('companies.csv')
+    output = open('link_list.txt', 'a+')
     reader = csv.reader(f)
     for row in reader:
-        activity2(row[1], row[1], 4)
-    
+        activity2(row[1], row[1], 4, f=output)
+
+    f.close()
+    output.close()
     print all_links
 
+
+
 if __name__ == '__main__':
-    #activity1()
+    activity1()
     #activity2("rit.edu", "https://www.rit.edu/", 4)
-    activity3()
+    #activity3()
 
